5001dbe6599f09a71f9e0735e1ace46efedba715
danthemanvsqz <danthemanvsqz@gmail.com>
2014-06-27 21:28:08 -0700

    Mongo Version

--- retr_val.py
+++ retr_val.py
@@ -1,19 +1,21 @@
 from flask import Flask, request, make_response
-import csv
+from flask.ext.pymongo import PyMongo
+
 app = Flask(__name__)
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def get_value():
     key = request.args.get('key')
-    if key:
-        with open('data.csv', 'rb') as csvfile:
-            data = { row[0] : row[1] for row in csv.reader(csvfile) }
-            value = data.get(key)
-            if value:
-                return make_response(value, 200)
-    return make_response('Not Found', 404)
+    doc = mongo.db.retr_val.find_one_or_404({"key": key})
+    value = doc["value"]
+    return make_response(value, 200)
     
     
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
+
+
